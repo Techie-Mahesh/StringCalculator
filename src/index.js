1d1d4f6f3cb5d91f69d4846b7a1c1f6e6b39bbe9
Techie-Mahesh <maheshTech1205@gmail.com>
2025-03-25 10:36:10 +0530

    Handle Multiple Numbers

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const calculator = new StringCalculator();
  
 console.log(calculator.add(""));
 console.log(calculator.add("1"));
+console.log(calculator.add("1,2"));
+console.log(calculator.add("1,2,3"));
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,18 +1,25 @@
 const StringCalculator = require("../src/calculator");
- 
+
 describe("String Calculator", () => {
   let calculator;
- 
+
   beforeEach(() => {
     calculator = new StringCalculator();
   });
- 
+
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
- 
+
   test("should return the number itself for a single number", () => {
     expect(calculator.add("1")).toBe(1);
   });
 
+  test("should return the sum of two comma-seperated numbers", () => {
+    expect(calculator.add("1,2")).toBe(3);
+  });
+
+  test("should return the sum of multiple comma-seperated numbers", () => {
+    expect(calculator.add("1,2,3")).toBe(6);
+  });
 });
